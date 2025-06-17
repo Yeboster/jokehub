a4bd2d199ae337457e74683b3d2622e53d0122be
Marco Vaccari <yeboster@gmail.com>
2025-06-17 13:16:18 +0000

    chore: add migration for joke ratings

---
+++ migrations/001-owner-ratings-to-joke-ratings.ts
@@ -0,0 +1,43 @@
+
+import { Joke } from "@/lib/types";
+import { Firestore, Timestamp } from 'firebase-admin/firestore';
+
+export default async function migrateOwnerRatings(db: Firestore) {
+  const jokesCollectionRef = db.collection("jokes");
+  const jokesSnapshot = await jokesCollectionRef.get()
+  const jokeRatingsCollectionRef = db.collection("jokeRatings");
+
+  for (const jokeDoc of jokesSnapshot.docs) {
+    console.log(`Processing joke ${jokeDoc.id}`)
+    const joke = jokeDoc.data() as Joke;
+    if (joke.funnyRate > 0) {
+      const existingRatingQuery = jokeRatingsCollectionRef
+        .where('jokeId', '==', jokeDoc.id)
+        .where('userId', '==', joke.userId)
+        .limit(1)
+      const existingRatingSnapshot = await existingRatingQuery.get();
+      if (existingRatingSnapshot.empty) {
+        const newRating = {
+          jokeId: jokeDoc.id,
+          userId: joke.userId,
+          rating: joke.funnyRate,
+          timestamp: Timestamp.now(),
+        };
+        await jokeRatingsCollectionRef.add(newRating);
+      }
+
+      // Update the average rating of the joke
+      const allRatingsQuery = jokeRatingsCollectionRef.where('jokeId', '==', jokeDoc.id);
+      const allRatingsSnapshot = await allRatingsQuery.get();
+      let totalRating = 0;
+      allRatingsSnapshot.docs.forEach(ratingDoc => {
+        totalRating += ratingDoc.data().rating;
+      });
+      await jokeDoc.ref.update({
+        averageRating: totalRating / allRatingsSnapshot.docs.length,
+        ratingCount: allRatingsSnapshot.docs.length,
+      });
+    }
+  }
+  console.log("Migration complete!");
+}
